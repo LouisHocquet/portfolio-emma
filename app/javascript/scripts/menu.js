4efc95380c3f281479af8d5a53ca54b85fcc55da
Louis Hocquet <louis.hocquet@edhec.com>
2022-02-19 17:58:08 +0100

    added overlay + stopped scrolling on opening menu

--- app/javascript/scripts/menu.js
+++ app/javascript/scripts/menu.js
@@ -4,6 +4,7 @@ const initMenu = () => {
     const navbarBurgerIcon = navbar.querySelector('.burger-icon');
     const menu = document.querySelector('.menu-fixed-container');
     const menuCloseIcon = menu.querySelector('.close-menu-icon');
+    const menuOverlay = document.querySelector('.menu-overlay');
 
     navbarBurgerIcon.addEventListener('click', () => {
       showMenu();
@@ -13,12 +14,20 @@ const initMenu = () => {
       hideMenu();
     });
 
+    menuOverlay.addEventListener('click', () => {
+      hideMenu();
+    });
+
     const showMenu = () => {
       menu.style.visibility = 'visible';
+      menuOverlay.style.visibility = 'visible';
+      document.body.classList.add('stop-scrolling');
     }
   
     const hideMenu = () => {
       menu.style.visibility = 'hidden';
+      menuOverlay.style.visibility = 'hidden';
+      document.body.classList.remove('stop-scrolling');
     }
   }
 }
