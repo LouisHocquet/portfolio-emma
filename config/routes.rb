4f05c953ff8ea4d0fd054ab8cfafcb24b7c01c7c
Louis Hocquet <louis.hocquet@edhec.com>
2022-03-13 20:53:50 +0100

    added file ssl correctly

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'work/:id', to: 'works#show', as: :work_show
   get 'category/:id', to: 'categories#show', as: :category_show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/.well-known/pki-validation/', :to => redirect('/89775073377839BF0B1B305FC2C8DE4F.txt')
+  # get '/.well-known/pki-validation/', :to => redirect('/89775073377839BF0B1B305FC2C8DE4F.txt')
 end
